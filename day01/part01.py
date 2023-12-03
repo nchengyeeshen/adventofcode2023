23252e94919ecf833ce7f6f6c28e7d28a92d378d
Nicholas Cheng <nchengyeeshen@gmail.com>
2023-12-03 09:30:30 +0800

    Day 1, part 1

---
+++ day01/part01.py
@@ -0,0 +1,21 @@
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("part01.py <input file>")
+        exit(1)
+
+    contents: list[str]
+    with open(sys.argv[1], "r") as f:
+        contents = f.readlines()
+
+    calibration_sum = 0
+    for line in contents:
+        digits = []
+        for char in line:
+            if char.isdigit():
+                digits.append(char)
+        calibration_sum += int(digits[0] + digits[-1])
+
+    print(calibration_sum)
