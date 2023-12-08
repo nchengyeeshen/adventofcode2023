f975bdfce236ce54d01e9b4815328061f9eb0443
Nicholas Cheng <nchengyeeshen@gmail.com>
2023-12-08 21:18:55 +0800

    Day 8, part 2
    I have no idea why the LCM thing works. I just read it on reddit.

--- day08/solve.py
+++ day08/solve.py
@@ -1,16 +1,9 @@
 import sys
+import math
+import functools
 
-if __name__ == "__main__":
-    with open("input.txt" if len(sys.argv) < 2 else sys.argv[1]) as f:
-        contents = f.read()
 
-    nodes = {}
-    steps, node_map = contents.split("\n\n")
-    for entry in node_map.splitlines():
-        node_id, edges = entry.split(" = ")
-        left, right = edges[1:-1].split(",")
-        nodes[node_id] = (left.strip(), right.strip())
-
+def part_one(steps, nodes):
     count = 0
     step_i = 0
     stack = ["AAA"]
@@ -26,4 +19,48 @@ if __name__ == "__main__":
         step_i += 1
         step_i %= len(steps)
 
-    print("Steps:", count)
+    return count
+
+
+def part_two(steps, nodes):
+    counts = []
+    starting_nodes = [node for node in nodes.keys() if node.endswith("A")]
+    for starting_node in starting_nodes:
+        count = 0
+        step_i = 0
+
+        first_z = None
+        stack = [starting_node]
+        while True:
+            node = stack.pop()
+
+            if node.endswith("Z") and first_z is None:
+                first_z = node
+                count = 0
+            elif node == first_z:
+                counts.append(count)
+                break
+
+            stack.append(nodes[node][0 if steps[step_i] == "L" else 1])
+
+            count += 1
+            step_i += 1
+            step_i %= len(steps)
+
+        counts.append(count)
+    return functools.reduce(math.lcm, counts)
+
+
+if __name__ == "__main__":
+    with open("input.txt" if len(sys.argv) < 2 else sys.argv[1]) as f:
+        contents = f.read()
+
+    nodes = {}
+    steps, node_map = contents.split("\n\n")
+    for entry in node_map.splitlines():
+        node_id, edges = entry.split(" = ")
+        left, right = edges[1:-1].split(",")
+        nodes[node_id] = (left.strip(), right.strip())
+
+    print("Part 1:", part_one(steps, nodes))
+    print("Part 2:", part_two(steps, nodes))
