7c015b22055bd518406c9828514e4a8e91155efb
Nicholas Cheng <nchengyeeshen@gmail.com>
2023-12-09 20:33:28 +0800

    Simplify

--- day09/solve.py
+++ day09/solve.py
@@ -1,4 +1,5 @@
-import sys
+from functools import reduce
+from sys import argv
 
 
 def extrapolate(values: list[int]) -> int:
@@ -7,23 +8,23 @@ def extrapolate(values: list[int]) -> int:
         seq = sequences[-1]
         if all(x == 0 for x in seq):
             break
-        differences = []
-        for i in range(1, len(seq)):
-            differences.append(seq[i] - seq[i - 1])
+        differences = [seq[i] - seq[i - 1] for i in range(1, len(seq))]
         sequences.append(differences)
+    return reduce(lambda acc, x: acc + x[-1], reversed(sequences[:-1]), 0)
 
-    v = 0
-    for i in range(len(sequences) - 2, -1, -1):
-        v = sequences[i][-1] + v
-    return v
+
+def part_one(values: list[list[int]]) -> int:
+    return sum(extrapolate(v) for v in values)
+
+
+def part_two(values: list[list[int]]) -> int:
+    return sum(extrapolate(list(reversed(v))) for v in values)
 
 
 if __name__ == "__main__":
-    filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+    filename = argv[1] if len(argv) > 1 else "input.txt"
     with open(filename, "r") as f:
         contents = f.read()
-
     values = [[int(v) for v in line.split()] for line in contents.splitlines()]
-    print("Part 1:", sum(extrapolate(v) for v in values))
-
-    print("Part 2:", sum(extrapolate(list(reversed(v))) for v in values))
+    print("Part 1:", part_one(values))
+    print("Part 2:", part_two(values))
